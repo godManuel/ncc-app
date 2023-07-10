dd83f7eebcbd9c6d26eb19dd551f37d4908c7b32
godManuel <emmanuelabraham375@gmail.com>
2023-07-10 22:01:04 +0100

    Refactored index.js to app.js

--- utils/sendOTP.js
+++ utils/sendOTP.js
@@ -1,12 +1,15 @@
+import twilio from "twilio";
 import dotenv from "dotenv";
 dotenv.config();
-import twilio from "twilio";
 
+// Declaring env variables in constants
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 
+// Initialize a twilio client
 const client = twilio(accountSid, authToken);
 
+// A SendOTP promise-based function
 const sendOTP = async (options) => {
   client.messages
     .create({
